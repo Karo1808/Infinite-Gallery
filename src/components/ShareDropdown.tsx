e6182b0d3bbb66fb2f994c8deafbbbc20eab6d83
Karol <karolp2003@gmail.com>
2024-02-19 11:56:50 +0100

    Testing link copy

--- src/components/ShareDropdown.tsx
+++ src/components/ShareDropdown.tsx
@@ -1,14 +1,17 @@
-import { FaFacebook, FaShare } from "react-icons/fa";
+import { FaFacebook, FaLink, FaShare } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import styles from "../styles/share-dropdown.module.css";
 import { useShare } from "react-facebook";
 import { useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 interface Props {
   imageLink: string;
 }
 
 const ShareDropdown = ({ imageLink }: Props) => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
   const location = useLocation();
   const { share } = useShare();
   const encodedURL = encodeURIComponent(imageLink);
@@ -33,26 +36,61 @@ const ShareDropdown = ({ imageLink }: Props) => {
     }
   };
 
+  const handleCopy = async () => {
+    const copyToClipboard = async () => {
+      try {
+        await navigator.clipboard.writeText(location.pathname);
+        setIsCopied(true);
+
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 2000);
+      } catch (error: unknown) {
+        if (error instanceof Error)
+          throw new Error(`Unable to copy text to clipboard, ${error.message}`);
+      }
+    };
+    await copyToClipboard();
+  };
+
   return (
-    <div className={styles.dropdown}>
-      <a
-        href={`https://twitter.com/intent/tweet?text=Check%20out%20this%20image!&url=${encodedURL}`}
-        target="_blank"
-        className={styles.entry}
+    <>
+      <motion.div
+        className={styles.dropdown}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.25 }}
       >
-        <FaXTwitter />
-        <p>X</p>
-      </a>
-      <div onClick={handleFbShare} className={styles.entry}>
-        <FaFacebook />
-        <p>Facebook</p>
-      </div>
-      {typeof navigator.share === "function" && (
-        <div onClick={handleMobileShare} className={styles.entry}>
-          <FaShare />
+        <a
+          href={`https://twitter.com/intent/tweet?text=Check%20out%20this%20image!&url=${encodedURL}`}
+          target="_blank"
+          className={styles.entry}
+        >
+          <FaXTwitter />
+          <p>X</p>
+        </a>
+        <div onClick={handleFbShare} className={styles.entry}>
+          <FaFacebook />
+          <p>Facebook</p>
         </div>
-      )}
-    </div>
+        {typeof navigator.share === "function" && (
+          <div onClick={handleMobileShare} className={styles.entry}>
+            <FaShare />
+            <p>Share</p>
+          </div>
+        )}
+        <div onClick={handleCopy} className={styles.entry}>
+          <FaLink />
+          <p>{isCopied ? "Copied" : "Copy"}</p>
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.25 }}
+        className={styles.triangle}
+      ></motion.div>
+    </>
   );
 };
 
